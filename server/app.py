fe2afcf0e9e5c1aceeb78d7ecaff3af82f2eb902
Abel Koech <koechabel161@gmail.com>
2024-10-05 20:53:04 +0300

    chat app

--- server/app.py
+++ server/app.py
@@ -14,13 +14,51 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        message_list = [message.to_dict() for message in messages]
+        return jsonify(message_list), 200
+    
+    elif request.method == 'POST':
+        data = request.json
+        body = data.get('body')
+        username = data.get('username')
+        if not body or not username:
+            return jsonify({"error": "Body and username are required"}), 400
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201 
+        # body = request.form.get('body')
+        # username = request.form.get('username')
+        # if not body or not username:
+        #     return jsonify({"error": "Body and username are required"}), 400
+        # new_message = Message(body=body, username=username)
+        # db.session.add(new_message)
+        # db.session.commit()
+        # return jsonify(new_message.to_dict()),201
+        
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == 'PATCH':
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+        
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {
+            "message": "message deleted successfuly."
+        }
+        return jsonify(response_body), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
